bda833a2cef1a8241eaab057c5385b66f224b809
CyberEzpertz <skyblaze24@gmail.com>
2025-07-28 10:43:13 +0800

    fix: remove unnecessaryt server action

--- src/actions/recommendations.ts
+++
@@ -1,7 +0,0 @@
-"use server";
-
-import { getRecommendedURLs } from "@/lib/fetchers";
-
-export async function getRecommendedMusic(chapterId: string) {
-  return await getRecommendedURLs(chapterId);
-}
